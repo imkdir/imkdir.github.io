d10fe02d91827ae97118522a2faa2714da5f35a5
imkdir <hichengd@gmail.com>
2026-03-02 17:02:22 +0800

    seo: add automated sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   site: "https://imkdir.com",
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), sitemap()],
 });
